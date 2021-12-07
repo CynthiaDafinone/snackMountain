3d9d2afdbbe5115cc8c682e599697d111d63f682
Cynthia Dafinone <c.dafinone@gmail.com>
2021-12-07 21:27:12 +0000

    started work on addRCS function this adds the candy to the inventory if you find it

--- src/gamefunctions.py
+++ src/gamefunctions.py
@@ -2,21 +2,52 @@
 import time
 
 def pickupRCS(player):
-    done ="Time's Up"
+
+    done ="Time's Up     "
     t = 60 #CONSTANT this is the what the timer needs to be.
-    cnt = 0
-    rCS = 0
+    c = 0
+    rcstotal = 0
+    tokenCount =""
+    found =""
     while t:
         mins, secs = divmod(t, 60)
-
-        timeCounter = " Token"
+         #the main counter
+        rcs = tokenTimer(c) #multiplier comes after this
 
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat+timeCounter, end='\r') #this just looks pretty / it's irrelevant really
+        found = " Found %d" % rcs
+        print(timeformat+found , end='\r') #The Output for current state Tokens and Stamina!
+
         time.sleep(1)#essentially the main coundown
         t -= 1 #essentially the main coundown
+        c += 1
+        rcstotal = rcstotal +rcs
+        
+    print (("You found %d Rock Candy Shards" %rcstotal))
+    
     return(done)
 
-
+#create a function to set players token
+#Create a dud print line to show real game output data
 
 # def addRCStoInventory
+
+def tokenTimer(c):
+    rcs = 0
+    if (c / 10 ).is_integer() and c>0:
+        rcs = 1
+
+        
+        
+    else: 
+        rcs =0
+        
+    return(rcs) 
+
+
+def staminaTimer():
+    pass
+
+
+def charachterMultiplier(rcs):
+    pass
